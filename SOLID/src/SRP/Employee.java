965a447797af3ce953506a203c10965b5e2bc54f
Molyanov Vladislav <molyanov1488@gmail.com>
2023-05-20 20:23:58 +0400

    add a solution to the problem according to principle Single Responsibility

---
+++ SOLID/src/SRP/Employee.java
@@ -0,0 +1,14 @@
+package SRP;
+
+import javax.xml.crypto.Data;
+
+public class Employee {
+    private String name;
+    private Data dob;
+    
+    public Employee(String name, Data dob) {
+        this.name = name;
+        this.dob = dob;
+    }
+    
+}
